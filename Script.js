e6fc0c6623f39c148e7768f86dda9dfe25d9b83d
ariiishaaaaaaaa <ariiishaaaaaaaa@gmail.com>
2025-01-23 21:31:26 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+function toggleContent(skill) {
+    var content = document.getElementById(skill);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
